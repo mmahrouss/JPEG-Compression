eeb90fdcf059b29b154d469f8f524ba1ded247f0
alaanousir <s-alaanousir@zewailcity.edu.eg>
2019-10-25 22:49:58 +0200

    reordered functions

--- decoder.py
+++ decoder.py
@@ -2,21 +2,61 @@ import numpy as np
 import pandas as pd 
 from huffman import encode as h_encode
 
-def get_reconstructed_image(divided_image, box_size = 8):
+def huffman_decode(huffcoded, code_dict):
     """
-    Gets an array of (box_size,box_size) pixels
-    and returns the reconstructed image
+    Decodes a string of a List of 0 and 1
+     (same choice for decoder and encoder)
     Args:
-        divided_image (numpy ndarray, dtype = "uint8"): array of divided images
-        box_size (int): Size of the box sub images
+        huffcoded : List or String of 0s and 1s code to be sent or stored
+        code_dict (dict): dict of symbol : code in binary
     Returns:
-        reconstructed_image (numpy ndarray): Image reconstructed from the array
-        of divided images.
+        rlcoded (numpy ndarray): 1d array
+    """
 
+
+
+def run_length_decode(rlcoded):
+    """
+    Returns 1D array of serialized dct values from an encoded 1D array.
+    Args:
+        rlcoded  (numpy ndarray): 1d array
+          Encoded in decimal not binary [Kasem]
+    Returns:
+        serialized (numpy ndarray): 1d array
+          has shape (X*box_size*box_size*n_channels,)
+    """
+
+
+
+def deserialize(serialized):
+    """
+    Removes serialization from quantized DCT values.
+    Args:
+        serialized (numpy ndarray): 1d array
+          has shape (X*box_size*box_size*n_channels,)
+    Returns:
+        quantized (numpy ndarray): array of quantized DCT values.
+          - should have a shape of (X, box_size, box_size, n_channels)
+           with dtype Int
     """
     
 
-    
+
+def dequantize(quantized, quantization_table):
+    """
+    Divides quantization table on DCT output
+    Args:
+        quantized (numpy ndarray): array of quantized DCT values
+        - should have a shape of (X, box_size, box_size, n_channels)
+         with dct applied to all of them
+        quantization_table (numpy ndarray): quantization table (matrix)
+        - should have a shape of (box_size, box_size)
+    Returns:
+        dct_values (numpy ndarray): array of DCT values.
+          same shape as dct_values but element type ints
+    """
+
+
 
 def idct(dct_values):
     """
@@ -43,55 +83,27 @@ def apply_idct_to_all(subdivded_dct_values):
     """
 
 
-def dequantize(quantized, quantization_table):
+
+def get_reconstructed_image(divided_image, box_size = 8):
     """
-    Divides quantization table on DCT output
+    Gets an array of (box_size,box_size) pixels
+    and returns the reconstructed image
     Args:
-        quantized (numpy ndarray): array of quantized DCT values
-        - should have a shape of (X, box_size, box_size, n_channels)
-         with dct applied to all of them
-        quantization_table (numpy ndarray): quantization table (matrix)
-        - should have a shape of (box_size, box_size)
+        divided_image (numpy ndarray, dtype = "uint8"): array of divided images
+        box_size (int): Size of the box sub images
     Returns:
-        dct_values (numpy ndarray): array of DCT values.
-          same shape as dct_values but element type ints
+        reconstructed_image (numpy ndarray): Image reconstructed from the array
+        of divided images.
+
     """
+    
+
     
 
 
 
-def deserialize(serialized):
-    """
-    Removes serialization from quantized DCT values.
-    Args:
-        serialized (numpy ndarray): 1d array
-          has shape (X*box_size*box_size*n_channels,)
-    Returns:
-        quantized (numpy ndarray): array of quantized DCT values.
-          - should have a shape of (X, box_size, box_size, n_channels)
-           with dtype Int
-    """
-    
 
 
-def run_length_decode(rlcoded):
-    """
-    Returns 1D array of serialized dct values from an encoded 1D array.
-    Args:
-        rlcoded  (numpy ndarray): 1d array
-          Encoded in decimal not binary [Kasem]
-    Returns:
-        serialized (numpy ndarray): 1d array
-          has shape (X*box_size*box_size*n_channels,)
-    """
 
-def huffman_decode(huffcoded, code_dict):
-    """
-    Decodes a string of a List of 0 and 1
-     (same choice for decoder and encoder)
-    Args:
-        huffcoded : List or String of 0s and 1s code to be sent or stored
-        code_dict (dict): dict of symbol : code in binary
-    Returns:
-        rlcoded (numpy ndarray): 1d array
-    """
+
+
