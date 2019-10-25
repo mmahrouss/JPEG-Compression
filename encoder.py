2b1949466363d7ab3395f43e846fd42735b3eefc
Mohamed Kasem <s-mohamed.kasem@zewailcity.edu.eg>
2019-10-25 23:16:12 +0200

    ِAfter fixing kasem's mess

--- encoder.py
+++ encoder.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import pandas as pd 
+from huffman import encode as h_encode
 
 def get_sub_images(image, box_size = 8):
     """
@@ -152,7 +153,6 @@ def run_length_code(serialized):
           Encoded in decimal not binary [Kasem]
     """
 
-
 def huffman_encode(rlcoded):
     """
     Encodes The run-length coded again with Huffman coding.
@@ -164,3 +164,7 @@ def huffman_encode(rlcoded):
         huffcoded : List or String of 0s and 1s code to be sent or stored
         code_dict (dict): dict of symbol : code in binary
     """
+    counts_dict = dict(pd.Series(rlcoded).value_counts)	
+    code_dict = h_encode(counts_dict)	
+    huffcoded = ''.join([code_dict[i] for i in rlcoded]) # list of strings to one joined string	
+    return huffcoded, code_dict
